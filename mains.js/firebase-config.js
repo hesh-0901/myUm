4d088a6a991574baf5a023b0141ebabca27cb3fe
Hesh <henochshungu@gmail.com>
2026-02-25 16:09:54 +0200

    Update firebase-config.js

--- mains.js/firebase-config.js
+++ mains.js/firebase-config.js
@@ -1,19 +1,23 @@
 // mains.js/firebase-config.js
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+// Configuration Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyC8SZu1X1dzqQya-1-_7wU8jfem2U6pWE0",
-  authDomain: "myum-54de2.firebaseapp.com",
-  projectId: "myum-54de2",
-  storageBucket: "myum-54de2.firebasestorage.app",
-  messagingSenderId: "33283961446",
-  appId: "1:33283961446:web:4711b0e410978668a573a7",
-  measurementId: "G-FF92444M2V"
+  apiKey: "AIzaSyDUuDQi3xzCT7lV1lOf3rhq724rfHhiTIQ",
+  authDomain: "umapp-72f36.firebaseapp.com",
+  projectId: "umapp-72f36",
+  storageBucket: "umapp-72f36.firebasestorage.app",
+  messagingSenderId: "483188283543",
+  appId: "1:483188283543:web:b52415f83bf21bad2071d2",
+  measurementId: "G-6X2ELMB8RY"
 };
 
+// Initialisation
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 export { db };
