78fab306b5bc1e45e2f825908c8390f0a182b82c
Hesh <henochshungu@gmail.com>
2026-02-25 15:16:14 +0200

    Create register.js

---
+++ mains.js/register.js
@@ -0,0 +1,52 @@
+import { auth } from "./firebase-config.js";
+import { createUserWithEmailAndPassword } 
+from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+const form = document.getElementById("registerForm");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const firstName = document.getElementById("firstName").value.trim();
+    const lastName = document.getElementById("lastName").value.trim();
+    const chorale = document.getElementById("chorale").value;
+    const phone = document.getElementById("phone").value.trim();
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+
+    if (password !== confirmPassword) {
+        alert("Les mots de passe ne correspondent pas.");
+        return;
+    }
+
+    try {
+        const email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@myum.com";
+
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        const user = userCredential.user;
+
+        console.log("Utilisateur créé :", user);
+
+        alert("Compte créé avec succès !");
+        window.location.href = "../index.html";
+
+    } catch (error) {
+        alert(error.message);
+    }
+});
+
+/* Toggle Password */
+document.querySelectorAll(".togglePassword").forEach(icon => {
+    icon.addEventListener("click", () => {
+        const input = icon.previousElementSibling;
+        if (input.type === "password") {
+            input.type = "text";
+            icon.classList.remove("bi-eye");
+            icon.classList.add("bi-eye-slash");
+        } else {
+            input.type = "password";
+            icon.classList.remove("bi-eye-slash");
+            icon.classList.add("bi-eye");
+        }
+    });
+});
