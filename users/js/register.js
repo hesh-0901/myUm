3cab1032388915e1b776172c441eba7b0f9004c6
Hesh <henochshungu@gmail.com>
2026-02-25 15:18:31 +0200

    Update register.js

--- users/js/register.js
+++ users/js/register.js
@@ -1,4 +1,4 @@
-import { auth } from "./firebase-config.js";
+import { auth } from "mains.js/firebase-config.js";
 import { createUserWithEmailAndPassword } 
 from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
