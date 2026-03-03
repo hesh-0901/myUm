9935bd43400b9d54ffcabd7321871cbec3cd0b77
Hesh <henochshungu@gmail.com>
2026-03-03 14:14:26 +0200

    Create header-back.js

---
+++ partials/js/header-back.js
@@ -0,0 +1,21 @@
+export function initBackHeader(customUrl = null) {
+
+  const btn = document.getElementById("globalBackBtn");
+  if (!btn) return;
+
+  btn.addEventListener("click", () => {
+
+    if (customUrl) {
+      window.location.href = customUrl;
+      return;
+    }
+
+    if (window.history.length > 1) {
+      window.history.back();
+    } else {
+      window.location.href = "/";
+    }
+
+  });
+
+}
