96729ca94a34c672cec092ce0ae6deea18bd80a2
Hesh <henochshungu@gmail.com>
2026-03-04 14:54:50 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,8 +2,11 @@ const CACHE_NAME = "myum-cache-v1";
 
 const urlsToCache = [
   "/myUm/",
-  "/myUm/index.html"
+  "/myUm/index.html",
+  "/myUm/assets/logo.png"
 ];
+
+/* INSTALL */
 
 self.addEventListener("install", event => {
 
@@ -12,21 +15,57 @@ self.addEventListener("install", event => {
       .then(cache => cache.addAll(urlsToCache))
   );
 
+  self.skipWaiting();
+
 });
+
+
+/* ACTIVATE */
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
+        keys.map(key => {
+
+          if(key !== CACHE_NAME){
+            return caches.delete(key);
+          }
+
+        })
+
+      );
+
+    })
+
+  );
+
+  self.clients.claim();
+
+});
+
+
+/* FETCH */
 
 self.addEventListener("fetch", event => {
 
   event.respondWith(
+
     caches.match(event.request)
       .then(response => {
 
-        if (response) {
+        if(response){
           return response;
         }
 
         return fetch(event.request);
 
       })
+
   );
 
 });
