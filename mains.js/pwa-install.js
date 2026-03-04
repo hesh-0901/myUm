11ab49ae0b9b7736e24ead27a3321d9658a38b3b
Hesh <henochshungu@gmail.com>
2026-03-04 14:52:43 +0200

    Create pwa-install.js

---
+++ mains.js/pwa-install.js
@@ -0,0 +1,76 @@
+let deferredPrompt;
+
+const modal = document.getElementById("installModal");
+const installBtn = document.getElementById("installBtn");
+const installText = document.getElementById("installText");
+const closeInstall = document.getElementById("closeInstall");
+
+
+function isIOS(){
+return /iphone|ipad|ipod/i.test(navigator.userAgent);
+}
+
+function isStandalone(){
+return window.matchMedia('(display-mode: standalone)').matches
+|| window.navigator.standalone === true;
+}
+
+
+/* ANDROID / WINDOWS */
+
+window.addEventListener("beforeinstallprompt",(e)=>{
+
+e.preventDefault();
+deferredPrompt = e;
+
+if(!isStandalone()){
+showModal();
+}
+
+});
+
+
+/* INSTALL CLICK */
+
+installBtn?.addEventListener("click", async ()=>{
+
+if(!deferredPrompt) return;
+
+deferredPrompt.prompt();
+
+await deferredPrompt.userChoice;
+
+hideModal();
+
+});
+
+
+/* IOS MESSAGE */
+
+window.addEventListener("load",()=>{
+
+if(isIOS() && !isStandalone()){
+
+installBtn.style.display="none";
+
+installText.innerHTML =
+'Sur iPhone :<br><br><b>Partager</b> <i class="bi bi-box-arrow-up"></i><br>puis <b>Ajouter à l’écran d’accueil</b>';
+
+showModal();
+
+}
+
+});
+
+
+closeInstall?.addEventListener("click", hideModal);
+
+
+function showModal(){
+modal?.classList.remove("hidden");
+modal?.classList.add("flex");
+}
+
+function hideModal(){
+modal?.classList.add("hidden");
+}
