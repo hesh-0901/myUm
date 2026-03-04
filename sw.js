8ef3dbb222ba00a139303c1edcc1df3b45a743f8
Hesh <henochshungu@gmail.com>
2026-03-04 14:19:45 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "myum-cache-v1";
+
+const urlsToCache = [
+  "/myUm/",
+  "/myUm/index.html"
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request);
+
+      })
+  );
+
+});
