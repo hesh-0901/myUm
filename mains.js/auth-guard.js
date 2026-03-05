c341f71d4b230cf4bb0d78c2ea678f09440fc14b
Hesh <henochshungu@gmail.com>
2026-03-05 10:22:08 +0200

    Update auth-guard.js

--- mains.js/auth-guard.js
+++ mains.js/auth-guard.js
@@ -2,12 +2,17 @@
 
 export function checkAuth() {
 
-  const storedUser = localStorage.getItem("myum_user");
+  // Vérifie localStorage (remember me) OU sessionStorage
+  const storedUser =
+    localStorage.getItem("myum_user") ||
+    sessionStorage.getItem("myum_user");
 
+  // Si aucun utilisateur trouvé → redirection login
   if (!storedUser) {
     window.location.href = "../users/login.html";
     return null;
   }
 
+  // Retourne les informations utilisateur
   return JSON.parse(storedUser);
 }
