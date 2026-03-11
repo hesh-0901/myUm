f00568b3316e43d744a43d6b9da2186ee48aeb10
Hesh <henochshungu@gmail.com>
2026-03-11 10:21:01 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    VERSIONING
 ========================= */
 
-const VERSION = "v21";
+const VERSION = "v22";
 
 const CACHE_STATIC = "myum-static-" + VERSION;
 const CACHE_DYNAMIC = "myum-dynamic-" + VERSION;
