bce2b25461f068146292e6e41436f987311c9488
Hesh <henochshungu@gmail.com>
2026-03-13 16:19:04 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    VERSIONING
 ========================= */
 
-const VERSION = "v30.1"; /*EnochMWALIMU==> amélioration du rendu audio et implementation des fonctionnalités audios premium,(listen speed, downloadanimation, audio visualiser */
+const VERSION = "v30.2"; /*henoch shungu */
 
 const CACHE_STATIC = "myum-static-" + VERSION;
 const CACHE_DYNAMIC = "myum-dynamic-" + VERSION;
