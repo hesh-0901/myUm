b1de5a595d7628f34c2c3c86db284fd525b651ba
Hesh <henochshungu@gmail.com>
2026-03-24 16:12:24 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    VERSIONING
 ========================= */
 
-const VERSION = "v1.35.6.0.8"; /*Enoch MWALIMU
+const VERSION = "v1.35.6.0.9"; /*Enoch MWALIMU
 découpage chat-room.js : fixing bugs d'affichage*/
 
 const CACHE_STATIC = "myum-static-" + VERSION;
