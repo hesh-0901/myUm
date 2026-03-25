bf487dcd3c2a11250a82b8f43ebd5798282be69b
peterhitchens <peterhitchens69@gmail.com>
2026-03-25 12:05:39 +0200

    Create maintenance-check.js

---
+++ chat/js/maintenance-check.js
@@ -0,0 +1,24 @@
+import { db } from "../../mains.js/firebase-config.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+export async function checkMaintenance(myId) {
+  try {
+    const snap = await getDoc(doc(db, "config", "app"));
+
+    if (!snap.exists()) return;
+
+    const data = snap.data();
+
+    const isMaintenance = data.chatMaintenance === true;
+
+    /* ⚠️ TOI = ADMIN */
+    const ADMIN_ID = "TON_ID_ICI";
+
+    if (isMaintenance && myId !== ADMIN_ID) {
+      window.location.href = "../maintenance.html";
+    }
+
+  } catch (e) {
+    console.error("Maintenance check error:", e);
+  }
+}
