695688a1971373c3010c03c36a9fb4bfba0ffc9c
Hesh <henochshungu@gmail.com>
2026-03-28 05:07:18 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    VERSIONING
 ========================= */
 
-const VERSION = "v1.35.6.1.3.2"; 
+const VERSION = "v1.35.6.1.3.3"; 
 /* 
 Enoch MWALIMU
 découpage chat-room.js : fixing bugs d'affichage
