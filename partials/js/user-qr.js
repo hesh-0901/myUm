3bc93a2f3376a2a731d86e52d31bfd5b886b0737
Hesh <henochshungu@gmail.com>
2026-03-31 10:53:24 +0200

    Create user-qr.js

---
+++ partials/js/user-qr.js
@@ -0,0 +1,37 @@
+import QRCode from "https://cdn.jsdelivr.net/npm/qrcode/build/qrcode.esm.min.js";
+
+export function initUserQr() {
+
+  const btn = document.getElementById("openUserQrBtn");
+  const modal = document.getElementById("userQrModal");
+  const closeBtn = document.getElementById("closeUserQr");
+  const canvas = document.getElementById("userQrCanvas");
+
+  if (!btn || !modal || !canvas) return;
+
+  btn.onclick = async () => {
+
+    const storedUser = JSON.parse(localStorage.getItem("myum_user"));
+
+    if (!storedUser) {
+      alert("Utilisateur non connecté");
+      return;
+    }
+
+    const userId = storedUser.id;
+
+    modal.classList.remove("hidden");
+
+    await QRCode.toCanvas(canvas, userId, {
+      width: 200
+    });
+
+  };
+
+  if (closeBtn) {
+    closeBtn.onclick = () => {
+      modal.classList.add("hidden");
+    };
+  }
+
+}
