f209467e5b56d255ec2bbc1e59b2e880865e04db
peterhitchens <peterhitchens69@gmail.com>
2026-04-02 10:29:57 +0200

    Create room-init.js

---
+++ chat/js/room/room-init.js
@@ -0,0 +1,51 @@
+import { getRoomDom } from "./room-ui.js";
+import { sendMessage } from "./room-service.js";
+import { listenMessages } from "./room-listener.js";
+
+/* SESSION */
+function getUser() {
+  try {
+    return JSON.parse(localStorage.getItem("myum_user"));
+  } catch {
+    return null;
+  }
+}
+
+const user = getUser();
+const myId = user?.id;
+
+if (!myId) {
+  alert("Session invalide");
+  location.href = "../../users/login.html";
+}
+
+/* PARAMS */
+const params = new URLSearchParams(window.location.search);
+const friendId = params.get("uid");
+
+if (!friendId) {
+  alert("Aucun utilisateur");
+  history.back();
+}
+
+/* INIT */
+export function initRoom() {
+  const dom = getRoomDom();
+
+  // envoyer message
+  dom.sendBtn.addEventListener("click", () => {
+    sendMessage(myId, friendId, dom.messageInput);
+  });
+
+  dom.messageInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      sendMessage(myId, friendId, dom.messageInput);
+    }
+  });
+
+  // écouter messages
+  listenMessages(myId, friendId, dom.messagesEl);
+}
+
+/* AUTO START */
+initRoom();
